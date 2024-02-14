51434a44ace2731ae5120be8b70cf29c99d4db78
Binit Shrestha <53391715+binitshrest@users.noreply.github.com>
2024-02-14 23:12:04 +0530

    made search pages and category field

--- 02-backend/spring-boot-library/src/main/java/com/binit/springbootlibrary/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-library/src/main/java/com/binit/springbootlibrary/config/MyDataRestConfig.java
@@ -1,11 +1,13 @@
 package com.binit.springbootlibrary.config;
 
 import com.binit.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     private String theAllowedOrigins = "http://localhost:3000";
 
--- 02-backend/spring-boot-library/src/main/java/com/binit/springbootlibrary/dao/BookRespository.java
+++
@@ -1,8 +0,0 @@
-package com.binit.springbootlibrary.dao;
-
-import com.binit.springbootlibrary.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRespository extends JpaRepository<Book, Long> {
-
-}
